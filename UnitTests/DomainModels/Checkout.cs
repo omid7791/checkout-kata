5920fb5af297d38c2b01079d1ecebe6584210c36
Omid S <omid.base@gmail.com>
2025-07-07 22:53:33 +0100

    feat(checkout): Pass the failing test for basket with only Item A

--- UnitTests/DomainModels/Checkout.cs
+++ UnitTests/DomainModels/Checkout.cs
@@ -2,13 +2,26 @@ namespace UnitTests.DomainModels;
 
 public class Checkout : ICheckout
 {
+    private readonly List<BasketItem> _basketItems = new List<BasketItem>();
+    
     public void Scan(string item)
     {
-        throw new NotImplementedException();
+        if (item == "A")
+        {
+            _basketItems.Add(new BasketItem { Item = item, Price = 50 });
+        }
     }
 
     public int GetTotalPrice()
     {
-        return 0;
+        var totalPrice = _basketItems.Sum(item => item.Price);
+
+        return totalPrice;
     }
+}
+
+public class BasketItem
+{
+    public string Item { get; set; }
+    public int Price { get; set; }
 }
