32e0728a2c2440b04580eac3ba004be38708fb3f
Omid S <omid.base@gmail.com>
2025-07-07 23:50:57 +0100

    feat(checkout): Pass the failing test for special pricing of Item A

--- UnitTests/DomainModels/Checkout.cs
+++ UnitTests/DomainModels/Checkout.cs
@@ -2,41 +2,111 @@ namespace UnitTests.DomainModels;
 
 public class Checkout : ICheckout
 {
-    private readonly List<BasketItem> _basketItems = new List<BasketItem>();
+    private readonly List<IBasketItem> _basketItems = [];
+
+    public Checkout()
+    {
+        
+    }
     
     public void Scan(string item)
     {
-        if (item == "A")
+        switch (item)
         {
-            _basketItems.Add(new BasketItem { Item = item, Price = 50 });
-        }
-        
-        if (item == "B")
-        {
-            _basketItems.Add(new BasketItem { Item = item, Price = 30 });
-        }
-        
-        if (item == "C")
-        {
-            _basketItems.Add(new BasketItem { Item = item, Price = 20 });
-        }
-        
-        if (item == "D")
-        {
-            _basketItems.Add(new BasketItem { Item = item, Price = 15 });
+            case "A":
+                if (_basketItems.All(basketItem => basketItem.ItemType != ItemType.A))
+                {
+                    _basketItems.Add(new BasketItemA { Count = 1 });
+                }
+                else
+                {
+                    _basketItems.Find(basketItem => basketItem.ItemType == ItemType.A)!.Count++;
+                }
+
+                break;
+            case "B":
+                _basketItems.Add(new BasketItemB());
+                break;
+            case "C":
+                _basketItems.Add(new BasketItemC());
+                break;
+            case "D":
+                _basketItems.Add(new BasketItemD());
+                break;
         }
     }
 
     public int GetTotalPrice()
     {
-        var totalPrice = _basketItems.Sum(item => item.Price);
+        var totalPrice = _basketItems.Sum(item => item.GetPrice());
 
         return totalPrice;
     }
 }
 
-public class BasketItem
+public class BasketItemA : IBasketItem
 {
-    public string Item { get; set; }
-    public int Price { get; set; }
+    public ItemType ItemType => ItemType.A;
+
+    public int GetPrice()
+    {
+        var inBatchesOfThree = Count / 3;
+        var remainder = Count % 3;
+        var totalPrice = (inBatchesOfThree * 130) + (remainder * 50);
+        
+        return totalPrice;
+    }
+
+    public int Count { get; set; }
+}
+
+public class BasketItemB : IBasketItem
+{
+    public ItemType ItemType => ItemType.B;
+
+    public int GetPrice()
+    {
+        return 30;
+    }
+
+    public int Count { get; set; }
+}
+
+public class BasketItemC : IBasketItem
+{
+    public ItemType ItemType => ItemType.C;
+
+    public int GetPrice()
+    {
+        return 20;
+    }
+
+    public int Count { get; set; }
+}
+
+public class BasketItemD : IBasketItem
+{
+    public ItemType ItemType => ItemType.D;
+
+    public int GetPrice()
+    {
+        return 15;
+    }
+
+    public int Count { get; set; }
+}
+
+public interface IBasketItem
+{
+    public ItemType ItemType { get; }
+    public int GetPrice();
+    public int Count { get; set; }
+}
+
+public enum ItemType
+{
+    A,
+    B,
+    C,
+    D
 }
